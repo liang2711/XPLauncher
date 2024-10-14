305ba62dd10caa891dd04ca8ea4a05ed7982bdb0
liang2711 <63349550+liang2711@users.noreply.github.com>
2024-10-14 13:43:24 +0800

    Create Theme.kt

---
+++ app/src/main/java/com/zhuoho/xplauncher/ui/theme/Theme.kt
@@ -0,0 +1,41 @@
+package com.zhuoho.xplauncher.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ProvidedValue
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.graphics.Color
+import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.darkColorScheme
+import androidx.tv.material3.lightColorScheme
+
+@OptIn(ExperimentalTvMaterial3Api::class)
+@Composable
+fun XPLauncherTheme(
+    isInDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit,
+) {
+    val colorScheme = if (isInDarkTheme) {
+        darkColorScheme(
+            primary = Purple80,
+            secondary = PurpleGrey80,
+            tertiary = Pink80
+        )
+    } else {
+        lightColorScheme(
+            primary = Purple40,
+            secondary = PurpleGrey40,
+            tertiary = Pink40
+        )
+    }
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+    ){
+        content()
+    }
+}
